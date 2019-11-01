feeded802af07ff2140e73466c7938c2ecf1509a
Javier Elio <javiereliomedina@gmail.com>
2019-11-01 12:28:23 +0100

    Clean

--- Rcode/5 Dose map.R
+++ Rcode/5 Dose map.R
@@ -1,18 +1,17 @@
-######################################################################
-####################         Dose map         ########################
-######################################################################
+
+# Dose map ----
 
   summary(Grids_10km_Sum$OK_AM)
   summary(Grids_10km_Sum$OK_SD)
 
-# New dataframe with AM and SD
+## New dataframe with AM and SD ----
   Dose <- Grids_10km_Sum %>% transmute(Id = Id,
                                        Rn_AM = OK_AM,
                                        Rn_SD = OK_SD,
                                        )
   
-# Dose [mSv/y] = CRn [Bq/m3] * FE * FO * TY [h/y] * FD [mSv / Bq.h.m-3]
-# Uncertainty MC simulations
+## Dose [mSv/y] = CRn [Bq/m3] * FE * FO * TY [h/y] * FD [mSv / Bq.h.m-3]
+  # Uncertainty MC simulations
   nsim <- 100
   MC_Sim <- matrix(NA, nrow = length(Dose$Rn_AM), ncol = nsim)
   TY <- 8760
@@ -28,11 +27,13 @@
   MC_Sim$Dose_AM <- rowMeans(MC_Sim[,1:nsim])
   MC_Sim$Dose_SD <- apply(MC_Sim[,1:nsim], 1, sd)
 
-# Add AM and SD of the MC simulations to the dose table 
-  Dose <- left_join(Dose %>% as.data.frame(), MC_Sim[c("Id","Dose_AM","Dose_SD")] %>% as.data.frame, by = "Id")
+## Add AM and SD of the MC simulations to the dose table ----
+  Dose <- left_join(Dose %>% as.data.frame(),
+                    MC_Sim[c("Id","Dose_AM","Dose_SD")] %>% as.data.frame,
+                    by = "Id")
   Dose <- Dose %>% st_sf(sf_column_name = "geometry.x")
   
-# Dose map
+## Dose map ----
   summary(Dose)
   
   P_Dose_AM <- ggplot() +
